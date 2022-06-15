5264b98bcb8c0fba66a91cc1484dcfd13bf69be5
selaguardia <selaguardia91@gmail.com>
2022-06-15 09:36:51 -0700

    function that returns a reverse string

---
+++ index.js
@@ -0,0 +1,19 @@
+//! June 12, 2022
+/* 
+This week’s exercise 
+
+Given a string of words, write a function that reverses all of the words within the string.
+Example:
+reverse("The greatest victory is that which requires no battle")
+"battle no requires which that is victory greatest The" 
+*/
+const OGstring = "The greatest victory is that which requires no battle"
+
+const reverseString = (string) => {
+  let arrayString = string.split(' ').reverse().join(' ');
+  console.log(`[OG String:] \n${OGstring}\n`)
+  console.log(`[Reversed String:] \n${arrayString}`);
+};
+reverseString(OGstring);
+
+
