ada9414be5590f7097ec2d27e06843ad0792265c
selaguardia <selaguardia91@gmail.com>
2022-08-22 11:22:26 -0700

    liter of water per hour of cycling

--- Practice/6-calculateWaterDranked.js
+++ Practice/6-calculateWaterDranked.js
@@ -1,6 +1,7 @@
 // Nathan loves cycling. Because Nathan knows it is important to stay hydrated, he drinks 0.5 liters of water per hour of cycling.
 // Write a function that takes in the number of hours and returns the number of liters Nathan will drink, rounded to the smallest value.
 const waterLitersPerHour = 0.5;
+
 const numOfLiters = (hours) => {
   return `${Math.floor(hours * waterLitersPerHour)} liter(s) of water consumed during a ${hours} hour cycling trip!`;
 }
