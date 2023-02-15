6bf028d5d2e1da4875bb90bea9adbc2e8ddd8fe3
selaguardia <selaguardia91@gmail.com>
2023-02-15 11:58:47 -0800

    adds practice problems

---
+++ DSA Qs/18-combineStrings.js
@@ -0,0 +1,17 @@
+/*
+Given a list of strings arr, and a max size n, return a new list where the strings (from left to right) are joined together with a space, so that each new string is less than or equal to the max size.
+Examples:
+> combineStrings(["a", "b", "c", "d", "e", "f", "g"], 5)
+["a b c", "d e f", "g"]
+
+> combineStrings(["a", "b", "c", "d", "e", "f", "g"], 12) 
+["a b c d e f", "g"]
+
+> combineStrings(["alpha", "beta", "gamma", "delta", "epsilon"], 20) 
+["alpha beta gamma", "delta epsilon"]`
+*/
+
+function combineStrings (stringArr, max) {
+
+}
+combineStrings(["a", "b", "c", "d", "e", "f", "g"], 5)
---
+++ DSA Qs/20-capitalAfterVowel.js
@@ -0,0 +1,9 @@
+/*
+Given a string, make every consonant after a vowel uppercase.
+Can you do this with and without regex?
+>capitalAfterVowel("hello world")
+> "heLlo WoRld"
+
+> capitalAfterVowel("xaabeuekadii")
+> "xaaBeueKaDii"
+*/
